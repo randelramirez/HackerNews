1e4a872c04dd4637898de5aa898d3d388a7a662e
Randel Ramirez <randelramirez1@gmail.com>
2020-01-15 23:42:14 +0800

    updated to asp.net core 3.1

--- HackerNews.WebAPI/Startup.cs
+++ HackerNews.WebAPI/Startup.cs
@@ -8,9 +8,11 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
 
 namespace HackerNews.WebAPI
 {
@@ -26,16 +28,22 @@ namespace HackerNews.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddMvc()
+                .AddJsonOptions(options =>
+                {
+                    //options.JsonSerializerOptions.WriteIndented = true;
+                });
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "Hacker News API (ASP.NET Core)", Version = "1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hacker News API (ASP.NET Core)", Version = "1" });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -47,11 +55,19 @@ namespace HackerNews.WebAPI
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+
+                // o serve the Swagger UI at the app's root (http://localhost:<port>/), set the RoutePrefix property to an empty string
+                c.RoutePrefix = string.Empty;
             });
         }
     }
